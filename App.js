0323bbd9c712e7dcea38e1440fa77b01b2d8f11b
ahamedkr <kadekabeer406@gmail.com>
2025-02-22 15:15:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import New from './new'
+import Header from './Header';
+import Footer from './footer';
+import Event from './event';
+import Clear from './praticks';
+import State from './praticks';
+import Fatch from './fetch';
+import Kabeer from './form';
+import Crazy from './form';
+import Setdata from './newfetch';
+import Externaldata from './newfetch';
+import Form from './newfetch';
+import SignUpForm from './form';
+import Valid from './indexs';
+
+function App() {
+  return (
+    <div className="App">
+      <Valid/>
+    </div>
+  );
+}
+
+export default App;
